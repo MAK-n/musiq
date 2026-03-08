c433554bce5022942ccf96ef41d32425d4a82e5b
Mohit Kumar <73781030+MAK-n@users.noreply.github.com>
2026-03-08 16:12:44 +0530

    Update UserRepository.java

--- backend/musiq/src/main/java/com/musiq/user/UserRepository.java
+++ backend/musiq/src/main/java/com/musiq/user/UserRepository.java
@@ -4,9 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserName(String userName);
-    boolean existsByUserName(String userName);   
+public interface UserRepository extends JpaRepository<User, Long> { 
     boolean existsBySpotifyId(String spotifyId);
     Optional<User> findBySpotifyId(String spotifyId);
+    Optional<User> findById(Long id);
 }
